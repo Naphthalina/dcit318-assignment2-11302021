efcc3a86af5dc71c2dc096c6a065dd2da3067f00
Naphthalina <bnassefuah@st.ug.edu.gh>
2025-08-07 17:11:23 -0400

    Final Implentation

--- Program.cs
+++ Program.cs
@@ -1,9 +1,38 @@
 ﻿using System;
+using dcit318_assignment2_11302021;
 
-public class Program
+namespace dcit318_assignment2_11302021
 {
-    public static void Main(string[] args)
+    class Program
     {
-        Animal myanimal = new Animal();
+        static void Main(string[] args)
+        {
+            // Inheritance and Method Overriding
+            Animal genericAnimal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+
+            Console.WriteLine();
+
+            // Abstract Classes and Methods
+            Shape circle = new Circle(14);
+            Shape rectangle = new Rectangle(10, 6);
+
+            Console.WriteLine($"Area of Circle: {circle.GetArea():F2}");
+            Console.WriteLine($"Area of Rectangle: {rectangle.GetArea():F2}");
+
+            Console.WriteLine();
+
+            // Interfaces
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
     }
 }
