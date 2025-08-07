6494a709b65e9f8455f04f49b89bcfe6be1f6c22
Naphthalina <bnassefuah@st.ug.edu.gh>
2025-08-07 17:03:03 -0400

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dcit318_assignment2_11302021
+{
+    public class Animal { 
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Make some genric sound");
+        }
+    }
+    
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+}
